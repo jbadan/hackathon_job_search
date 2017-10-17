0aceb588e44177ca994d88042479385865060d74
jbadan <jenna.badanowski@gmail.com>
2017-10-17 12:36:52 -0700

    blah

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -11,9 +11,18 @@ const styles = {
 };
 
 class Profile extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      list: ''
+    }
+  }
+  handleChangeValue = e => this.setState({
+    list: e.target.value
+  })
   componentDidMount() {
      WordCloud(this.refs["my-canvas"], {
-       list: [["foo", 20], ["bar", 6]],
+       list: this.state.list,
        weightFactor: 5,
        fontFamily: "Times, serif",
        color: function(word, weight) {
@@ -27,7 +36,10 @@ class Profile extends Component {
   render() {
     return (
       <div className="App">
-           <TextInput />
+           <TextInput
+              value={this.state.list}
+              onChangeValue={this.handleChangeValue}
+            />
            <div style={styles}>
               <canvas ref="my-canvas" />
           </div>
