9ff27fe9187ab8a56d1b5a7cc2302d5c0ac97bc2
jbadan <jenna.badanowski@gmail.com>
2017-10-17 11:26:39 -0700

    added router/routes

---
+++ client/src/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="App">
+           <TextInput />
+      </div>
+    );
+  }
+}
+export default Profile;
