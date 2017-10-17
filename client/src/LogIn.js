e48d7a879cc68e6d2bf280024e62339c3b4e1846
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-10-17 12:28:59 -0700

    set up login form, still need to hook up to user db

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class LogIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    console.log('A user has been logged in' + this.state.value);
+    event.preventDefault();
+  }
   render() {
     return (
-      <div className="App">
-        <h1> this is the login page </h1>
+      <div id='signin'>
+      <h1>Please login to get started!</h1>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <label>
+            Password:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+         </label>
+        <input type='submit' value='Login' />
+      </form>
       </div>
     );
   }
